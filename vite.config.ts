2fcb9f6441a5118554fc46ea197608ccc6bf4f58
Jeet Patel <Jeetpatel1011@gmail.com>
2024-03-15 16:41:10 -0400

    fixed URL path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/chronosync-server",
+  base: "/chronosync-client",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
